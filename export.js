cfa129e3610deb60588d6db103f3d98577362242
unknown <munnamukesh152@gmail.com>
2022-03-15 14:37:01 +0530

    08-03-2022

---
+++ export.js
@@ -0,0 +1,14 @@
+export function area(a,b)
+{
+    return a*b;
+}
+function perimeter(a,b)
+{
+    return 2*(a+b);
+}
+function sum(a,b)
+{
+    return a+b;
+}
+export {sum}
+export default perimeter
---
+++ import.js
@@ -0,0 +1,6 @@
+import s,{sum,area} from "./export"
+var a=1;
+var b=2;
+console.log(sum(a,b));
+console.log(s(a,b));
+console.log(area(a,b));
